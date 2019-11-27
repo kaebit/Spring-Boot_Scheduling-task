815cef361a6cf756db3891687fdae557a616a768
kaebit <kaibee@kakao.com>
2019-11-27 23:25:16 +0900

    Initial commit

---
+++ src/main/java/com/kaebit/schedulingtask/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package com.kaebit.schedulingtask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 1000) // set the Run interval time(ex: 1000 -> 1sec)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
